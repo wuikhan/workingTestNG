56c383a072c1af67d177f925099cd108e87d17fc
wuikhan <wuikhan@gmail.com>
2018-12-27 17:42:46 -0500

    initial commit

---
+++ src/test/java/com/maven/BaseClass.java
@@ -0,0 +1,24 @@
+package com.maven;
+
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.testng.annotations.Test;
+
+public class BaseClass {
+	
+	@Test
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "/Users/waqaskhan/Documents/JAR Files/chromedriver");
+		WebDriver  driver = new ChromeDriver();
+//	    driver.manage().window().fullscreen();
+	    driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+	    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	    driver.get("https://opensource-demo.orangehrmlive.com/");
+// this is a working code this will open browser in chrome driver
+	}
+
+}
---
+++ src/test/java/com/maven/MavenTest.java
@@ -0,0 +1,13 @@
+package com.maven;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class MavenTest {
+	
+	@Test
+	public void hello() {
+		System.out.println("This is my first maven test");
+	}
+
+}
